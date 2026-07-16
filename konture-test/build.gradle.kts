533e2a564f7c45ca96aa4770c5fa655054639476
baole <leducbao@gmail.com>
2026-07-16 21:11:18 +0200

    Bump Konture version to 0.6.10

--- konture-test/build.gradle.kts
+++ konture-test/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     // The unified Konture library
-    testImplementation("io.github.baole:konture:0.6.9")
+    testImplementation("io.github.baole:konture:0.6.10")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
