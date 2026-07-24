9dbc619715076ab4073eb8f630a0e12cad8fa300
baole <leducbao@gmail.com>
2026-07-24 17:40:20 +0200

    Bump Konture version to 0.7.3

--- konture-test/build.gradle.kts
+++ konture-test/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     // The unified Konture library
-    testImplementation("io.github.baole:konture:0.7.2")
+    testImplementation("io.github.baole:konture:0.7.3")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
